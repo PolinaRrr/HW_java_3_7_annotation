c2abe77606fa6a76010d41c4eb723d98c7c847a9
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-15 15:18:20 +0300

    Commit#2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -8,6 +9,40 @@ public class Main {
 
         Method[] methods = testClass.getDeclaredMethods();
         ArrayList<Method> listMethods = new ArrayList<>();
-        
+
+        Method beforeSuite = null;
+        Method afterSuite = null;
+
+        for (Method m : testClass.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(Test.class)) {
+                listMethods.add(m);
+            }
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                if (beforeSuite == null) {
+                    beforeSuite = m;
+                } else throw new RuntimeException("The code contains several methods with the @BeforeSuite annotation");
+            }
+            if (m.isAnnotationPresent(AfterSuite.class)) {
+                if (afterSuite == null) {
+                    afterSuite = m;
+                } else throw new RuntimeException("The code contains several methods with the @AfterSuite annotation");
+            }
+            listMethods.sort(new Comparator<Method>() {
+                @Override
+                public int compare(Method o1, Method o2) {
+                    return o2.getAnnotation(Test.class).priority() - o1.getAnnotation(Test.class).priority();
+                }
+            });
+
+        }
+        if (beforeSuite != null) {
+            beforeSuite.invoke(testObject, null);
+        }
+        for (Method m : listMethods) {
+            m.invoke(testObject, null);
+        }
+        if (afterSuite != null) {
+            afterSuite.invoke(testObject, null);
+        }
     }
 }
